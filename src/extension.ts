7b03b18cfaa7e5aa6974caf7cf014056e47bb45b
neet <n33t5hin@gmail.com>
2018-09-19 21:45:58 +0900

    context.subscriptionsを利用

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,28 @@
-import { commands, window /* , ExtensionContext */} from 'vscode';
+import { commands, ExtensionContext, window } from 'vscode';
 import * as nls from 'vscode-nls';
 import { compose } from './commands/compose';
 import { deleteItem } from './commands/deleteItem';
 import { editTags } from './commands/editTags';
 import { makePublic } from './commands/makePublic';
+import { openItem } from './commands/openItem';
 import { openItemExternal } from './commands/openItemExternal';
 import { QiitaItemProvider } from './explorer/qiitaItems';
 import './polyfills';
 
 nls.config(process.env.VSCODE_NLS_CONFIG as nls.Options)();
 
-export function activate (/*context: ExtensionContext */) {
+export function activate (context: ExtensionContext) {
+  console.log(context);
   window.registerTreeDataProvider('qiitaItems', new QiitaItemProvider());
 
-  commands.registerCommand('qiita.openItem', () => undefined);
-  commands.registerCommand('qiita.editTags', editTags);
-  commands.registerCommand('qiita.makePublic', makePublic);
-  commands.registerCommand('qiita.compose', compose);
-  commands.registerCommand('qiita.openItemExternal', openItemExternal);
-  commands.registerCommand('qiita.deleteItem', deleteItem);
+  context.subscriptions.push(
+    commands.registerCommand('qiita.openItem', openItem(context.storagePath)),
+    commands.registerCommand('qiita.editTags', editTags),
+    commands.registerCommand('qiita.makePublic', makePublic),
+    commands.registerCommand('qiita.compose', compose),
+    commands.registerCommand('qiita.openItemExternal', openItemExternal),
+    commands.registerCommand('qiita.deleteItem', deleteItem),
+  );
 }
 
 export function deactivate () {
