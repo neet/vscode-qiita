ae213f470e40d99d22b43283a3cb9525d6a19280
neet <n33t5hin@gmail.com>
2018-09-19 22:20:01 +0900

    Prettify

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -11,16 +11,34 @@ const localize = nls.loadMessageBundle();
 export function handleErrorMessage <T extends QiitaError> (error: T) {
   switch (error.name) {
     case 'QiitaUnauthorizedError':
-      return window.showErrorMessage(localize('general.failure.unauhtorized', 'リクエストに失敗しました。Qiitaアカウントを認証しているかお確かめの上もう一度お試しください'));
+      return window.showErrorMessage(localize(
+        'general.failure.unauhtorized',
+        'リクエストに失敗しました。Qiitaアカウントを認証しているかお確かめの上もう一度お試しください',
+      ));
     case 'QiitaForbiddenError':
-      return window.showErrorMessage(localize('general.failure.forbitten', 'リクエストに失敗しました。正しいアカウントにログインしているかをお確かめの上もう一度お試しください'));
+      return window.showErrorMessage(localize(
+        'general.failure.forbitten',
+        'リクエストに失敗しました。正しいアカウントにログインしているかをお確かめの上もう一度お試しください',
+      ));
     case 'QiitaNotFoundError':
-      return window.showErrorMessage(localize('general.failure.notFound', 'リクエストに失敗しました。指定されたページは存在しないか削除された可能性があります。'));
+      return window.showErrorMessage(localize(
+        'general.failure.notFound',
+        'リクエストに失敗しました。指定されたページは存在しないか削除された可能性があります。',
+      ));
     case 'QiitaRateLimitError':
-      return window.showErrorMessage(localize('general.failure.rateLimit', 'APIのレートリミットに到達しました。時間をおいてもう一度お試しください。'));
+      return window.showErrorMessage(localize(
+        'general.failure.rateLimit',
+        'APIのレートリミットに到達しました。時間をおいてもう一度お試しください。',
+      ));
     case 'QiitaInternalServerError':
-      return window.showErrorMessage(localize('general.failure.InternalServerError', '削除に失敗しました。Qiitaのサーバーがダウンしている可能性があります。'));
+      return window.showErrorMessage(localize(
+        'general.failure.InternalServerError',
+        '削除に失敗しました。Qiitaのサーバーがダウンしている可能性があります。',
+      ));
     default:
-      return window.showErrorMessage(localize('general.failure.fallback', 'ご迷惑をお掛けしてしまい申し訳ございませんが、エラーが発生しました。 バグの可能性がある場合は開発者にご報告いただけると幸いです。\nhttps://github.com/neet/vscode-qiita/issues'));
+      return window.showErrorMessage(localize(
+        'general.failure.fallback',
+        'ご迷惑をお掛けしてしまい申し訳ございませんが、エラーが発生しました。 バグの可能性がある場合は開発者にご報告いただけると幸いです。\nhttps://github.com/neet/vscode-qiita/issues',
+      ));
   }
 }
