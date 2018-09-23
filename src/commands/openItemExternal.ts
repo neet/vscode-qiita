8b9522fe5a171bb5165fa6b10919b1c674661903
neet <n33t5hin@gmail.com>
2018-09-23 23:56:34 +0900

    openItemExternalにTSDocを追加

--- src/commands/openItemExternal.ts
+++ src/commands/openItemExternal.ts
@@ -1,6 +1,10 @@
 import { Item } from 'qiita-js-2';
 import { commands, Uri } from 'vscode';
 
+/**
+ * アイテムをブラウザで表示
+ * @param resource コマンドから渡される引数
+ */
 export function openItemExternal (resource: { item: Item }) {
   const { url } = resource.item;
   commands.executeCommand('vscode.open', Uri.parse(url));
