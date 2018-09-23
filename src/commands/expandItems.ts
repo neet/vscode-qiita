07be3ed33d909c98d68da9cd5961b9a0d30bc7f3
neet <n33t5hin@gmail.com>
2018-09-23 23:54:03 +0900

    expandItemsにエラーハンドリングを追加

--- src/commands/expandItems.ts
+++ src/commands/expandItems.ts
@@ -1,11 +1,12 @@
 import { qiitaItemsProvider } from '../explorers/qiitaItems';
+import { handleErrorMessage } from '../utils/errorHandler';
 
 export async function expandItems () {
   try {
     if (!qiitaItemsProvider.done) {
       await qiitaItemsProvider.expandItems();
     }
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    handleErrorMessage(error);
   }
 }
